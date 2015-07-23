e68eb2f37cfbadb0dc8a175dd74b5b6b3c363bc5
jzhauzel <jzhauzel@gmail.com>
2015-07-23 18:35:00 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My functions cache a matrix and it's inverse, returns the inverse,
+## and let you know whether or not the matrix was
+## already in the cache.
 
-## Write a short comment describing this function
+## This function takes a matrix and creates a global variable "mat",
+## assigning it the value of the matrix and creates and assigns a global variable
+## "inverse" to the inverse of the matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+	mat <<- x
+	inverse <<- solve(mat)
 }
 
+## This function first determines if a matrix has already been cached
+## to the global environment and if so, checks if its argument matches
+## the cached matrix. Then, if the two match, produces the already computed
+## inverse of the matrix. If the two matrices don't match or if a matrix has
+## yet to be cached, then the function returns the inverse of the matrix.
+## The function also tell you whether or not the matrix inverse was already cached.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){
+	if(exists("mat") == TRUE){
+		compare <- identical(x,mat)
+		if(compare == TRUE){
+			print("getting cached data")
+			return(inverse)
+		}
+		else{
+			print("no cached data")
+			return(solve(x))
+		}
+	}
+	else{
+		print("no cached data")
+		return(solve(x))
+	}
 }
