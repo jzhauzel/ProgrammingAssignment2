870603881c4f871d65f4fe60584703bdcf1b9256
jzhauzel <jzhauzel@gmail.com>
2015-11-20 17:38:36 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,9 @@
 ## and let you know whether or not the matrix was
 ## already in the cache.
 
+## The first function creates a special matrix which is a list that contains functions to 
+## set/get/set inverse/get inverse values of a mtrix.
+
 makeCacheMatrix <- function(x = matrix()){
 	m <- NULL
         set <- function(y) {
@@ -15,6 +18,10 @@ makeCacheMatrix <- function(x = matrix()){
              setinverse = setinverse,
              getinverse = getinverse)
 }
+
+# This second function calculates the value of the inverse of the matrix, but first
+# checks to see if it has already been done. If so it returns the value and if not 
+# calculates the value of the inverse and then returns it.
 
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
