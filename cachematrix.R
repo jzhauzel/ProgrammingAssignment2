e4d748bd29a7ddf2cf62d6a7d5e0ab1ab938ec6c
jzhauzel <jzhauzel@gmail.com>
2015-11-20 17:31:49 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,10 +2,6 @@
 ## and let you know whether or not the matrix was
 ## already in the cache.
 
-## This function takes a matrix and creates a global variable "mat",
-## assigning it the value of the matrix and creates and assigns a global variable
-## "inverse" to the inverse of the matrix.
-
 makeCacheMatrix <- function(x = matrix()){
 	m <- NULL
         set <- function(y) {
@@ -19,13 +15,6 @@ makeCacheMatrix <- function(x = matrix()){
              setinverse = setinverse,
              getinverse = getinverse)
 }
-
-## This function first determines if a matrix has already been cached
-## to the global environment and if so, checks if its argument matches
-## the cached matrix. Then, if the two match, produces the already computed
-## inverse of the matrix. If the two matrices don't match or if a matrix has
-## yet to be cached, then the function returns the inverse of the matrix.
-## The function also tell you whether or not the matrix inverse was already cached.
 
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
